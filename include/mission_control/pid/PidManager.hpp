a245dd1d42e416ce3ff45a93420662661160010d
seth <sisaacson@hmc.edu>
2019-01-24 03:02:57 -0500

    started working on axis class to house operations on an individual axis. For clarity, renamed PidAxis -> Pid

---
+++ include/mission_control/pid/PidManager.hpp
@@ -0,0 +1,19 @@
+#ifndef PID_MANAGER
+#define PID_MANAGER
+
+#include <PidUtils.hpp>
+
+class PidManager{
+
+
+private:
+
+  UpdateController(const PidUtils::Axes& axis, const std::string& param, const double& val);
+
+  CheckStability()
+
+}
+
+class Axis
+
+#endif
