0c58d9977c16b04937e46cf36aaf69d3984be3d0
seth <sisaacson@hmc.edu>
2019-02-25 17:11:57 -0500

    baseline pid manager working

--- include/mission_control/pid/PidManager.hpp
+++ include/mission_control/pid/PidManager.hpp
@@ -6,18 +6,37 @@
 #ifndef PID_MANAGER
 #define PID_MANAGER
 
-#include <PidUtils.hpp>
+#include "mission_control/pid/PidUtils.hpp"
+#include "mission_control/Axis.hpp"
+#include <ros/ros.h>
 
 class PidManager{
 
 
+public:
+
+  PidManager() = delete;
+  PidManager(ros::NodeHandle);
+  void setPidEnabled(const PidUtils::Axes& axis, const bool& enabled = true);
+  void setPlantState(const PidUtils::Axes& axis, const double& val);
+  void setSetpoint(const PidUtils::Axes& axis, const double& val);
+  void setInputType(const PidUtils::Axes& axis, const PidUtils::Inputs&);
+
 private:
 
-  UpdateController(const PidUtils::Axes& axis, const std::string& param, const double& val);
+  Axis& selectAxis(const PidUtils::Axes& axis);
+  void setPercentThrust(const PidUtils::Axes& axis, const double& val);
 
-  CheckStability()
 
-}
+
+  void CheckStability();
+
+  Axis axisSurge_, axisSway_, axisHeave_, axisRoll_, axisPitch_, axisYaw_;
+
+  ros::NodeHandle nh_;
+
+
+};
 
 
 #endif
--- src/Axis.cpp
+++ src/Axis.cpp
@@ -1,7 +1,11 @@
 #include "../include/mission_control/Axis.hpp"
+//
+// Axis::Axis(){
+//   std::cerr << "boo" << std::endl;
+// }
 
 
-Axis::Axis(std::string axisName, ros::NodeHandle nh)
+Axis::Axis(std::string axisName, const ros::NodeHandle& nh)
     : plantState_(0), setpoint_(0), controlEffort_(0), nh_(nh)
 {
 
@@ -11,12 +15,13 @@ Axis::Axis(std::string axisName, ros::NodeHandle nh)
 
   controlEffortTopic_ = axisName + "ControlEffort";
   percentThrustTopic_ = axisName + "PercentThrust";
-  plantStateTopic_ = axisName = "PlantState";
-
+  plantStateTopic_ = axisName + "PlantState";
+  std::cout << plantStateTopic_ << std::endl;
   plantPub_ = nh_.advertise<std_msgs::Float64>(plantStateTopic_, 1);
 
 
 }
+
 
 void Axis::updateController(const PidUtils::UpdateParams& param, const double& val){
 
@@ -55,15 +60,15 @@ void Axis::setPlantState(const double& val){
   plantPub_.publish(msg);
 }
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "Axis");
-  ros::NodeHandle nh_;
-  Axis a("hHaAFDi", nh_);
-  ROS_INFO("YEEET");
-  double b = 11;
-  a.updateController(PidUtils::SETPOINT, b);
-  for(int i = 0; i < 100000; ++i){
-    ROS_INFO("Hmm");
-    a.setPlantState(i);
-  }
-}
+// int main(int argc, char** argv){
+//   ros::init(argc, argv, "Axis");
+//   ros::NodeHandle nh_;
+//   Axis a("hHaAFDi", nh_);
+//   ROS_INFO("YEEET");
+//   double b = 11;
+//   a.updateController(PidUtils::SETPOINT, b);
+//   for(int i = 0; i < 100000; ++i){
+//     ROS_INFO("Hmm");
+//     a.setPlantState(i);
+//   }
+// }
